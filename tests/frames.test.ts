a3ae014bffb8e974e97475725699c499ca7be961
Priyansh <bobbyskhs@gmail.com>
2022-01-20 21:40:47 -0500

    style: add tests for empty header

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -17,6 +17,8 @@ const header = ['a', 'b', 'c'];
 
 const changedHeader = ['j', 'k', 'l'];
 
+const defaultHeader = ['Column 1', 'Column 2', 'Column 3'];
+
 describe('frames.test.ts', () => {
   describe('Create a new frame with column names', () => {
     it('should create a new frame', () => {
@@ -36,6 +38,16 @@ describe('frames.test.ts', () => {
       frame.header(changedHeader);
       expect(frame.rowdata).to.deep.equal(changedData);
       expect(frame.columns).to.deep.equal(changedHeader);
+    });
+  });
+  describe('Remove Header', () => {
+    it('should reset the header to default', () => {
+      const frame = new Frame(data);
+      frame.header(header);
+      expect(frame.rowdata).to.deep.equal(data);
+      expect(frame.columns).to.deep.equal(header);
+      frame.header();
+      expect(frame.columns).to.deep.equal(defaultHeader);
     });
   });
 });
